564be4d6d81c4f7a8e113ac92e55450b45a9f093
@brindha.e <brindha.e@thoughtworkworks.com>
2022-07-09 22:32:35 +0530

    Add ref instead of useState while getting new user

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState,useRef} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
@@ -6,44 +6,51 @@ import ErrorModal from '../UI/ErrorModal';
 
 
 const AddUser = (props) => {
-    const [userData,setUserData] = useState({
-        userName: '',
-        age: ''
-    })
+    // const [userData,setUserData] = useState({
+    //     userName: '',
+    //     age: ''
+    // })
     const [error, setError] = useState(false);
+
+    const nameRef =useRef();
+    const ageRef = useRef();
+
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(userData.userName.trim().length === 0 || userData.age.trim().length === 0) {
+        if(nameRef.current.value.trim().length === 0 || ageRef.current.value.trim().length === 0) {
             setError({
                 title: 'Invalid Input',
                 message: 'Please enter a valid name and age (non-empty values)'
             });
             return;
         }
-        if(+userData.age < 1) {
+        if(+ageRef.current.value < 1) {
             setError({
                 title: 'Invalid Age',
                 message: 'Please enter a valid age (> 0)'
             });
             return;
         }
-        setUserData({userName:'', age: ''});
-        const user = {userName:userData.userName,age:userData.age,id:Math.random.toString()}
+        // setUserData({userName:'', age: ''});
+        const user = {userName:nameRef.current.value,age:ageRef.current.value,id:Math.random.toString()}
         props.addUser(user);
+        nameRef.current.value = '';
+        ageRef.current.value = '';
+
     }
 
-    const onChangeUserName = (event) => {
-        setUserData((prevData) => {
-            return {...prevData,userName:event.target.value};
-        });
-    }
+    // const onChangeUserName = (event) => {
+    //     setUserData((prevData) => {
+    //         return {...prevData,userName:event.target.value};
+    //     });
+    // }
 
-    const onChangeAge = (event) => {
-        setUserData((prevData) => {
-            return {...prevData,age:event.target.value};
-        });
-    }
+    // const onChangeAge = (event) => {
+    //     setUserData((prevData) => {
+    //         return {...prevData,age:event.target.value};
+    //     });
+    // }
 
     const errorHandler = () => {
         setError(null);
@@ -55,9 +62,9 @@ const AddUser = (props) => {
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" value={userData.userName} onChange={onChangeUserName} />
+        <input id="username" type="text" ref={nameRef} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" value={userData.age} onChange={onChangeAge}/>
+        <input id="age" type="number" ref={ageRef}/>
         <Button type="submit">Add User</Button>
     </form>
     </Card>
